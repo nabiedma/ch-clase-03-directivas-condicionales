3945af7c02e77f285bcd2103722e341c34b6b3fb
nabiedma <nabiedma@gmail.com>
2021-11-15 22:08:10 -0300

    commit tabla

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: 'Alejandro Biedma',
+        productos: [
+                {
+                    id: 1,
+                    nombre: "Leche",
+                    precio: 110
+                },
+                {
+                    id: 2,
+                    nombre: "Huevos",
+                    precio: 65
+                },
+                {
+                    id: 3,
+                    nombre: "Pan de Campo",
+                    precio: 125
+                },
+                {
+                    id: 4,
+                    nombre: "Jugo",
+                    precio: 140
+                }
+        ],
+    },
+    methods: {
+
+    },
+  })
